05261df5352cfded41656c605d35da8cecdbbbc8
711029 <51910260+711029@users.noreply.github.com>
2020-06-01 15:19:53 +0800

    初步配置

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import fastclick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/bordor.css'
 
 Vue.config.productionTip = false
+fastclick.attach(document.body)
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
