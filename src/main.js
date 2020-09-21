5b302da616a6fc14563cf7bfec4c86d9ea47e4e7
Coder <3170495286@qq.com>
2020-09-21 18:34:24 +0800

    update src/main.js.

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ import axios from 'axios'
 import Bmob from 'hydrogen-js-sdk'
 
 // 初始化
-Bmob.initialize('5baa52ee149cc61c','123456','5a5681343d697a4ea0b6364166fe003c')
+Bmob.initialize("你的Application ID","你的REST API Key","你的MasterKey");
 // 挂载到全局使用
 Vue.prototype.Bmob = Bmob
 // 挂载到全局使用
